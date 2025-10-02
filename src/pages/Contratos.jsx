92289fcc5eda4aac0c185d9d5b0a5a2f528397c1
Juan Mateo HDL <22300015@uttt.edu.mx>
2025-10-02 16:53:56 -0600

    fix: Correción de errores de vistas vacias

---
+++ src/pages/Contratos.jsx
@@ -0,0 +1,3 @@
+export default function Empleados() {
+  return <h1 className="text-xl font-bold">Módulo: Contratos</h1>
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,3 @@
+export default function Empleados() {
+  return <h1 className="text-xl font-bold">Módulo: Home</h1>
+}

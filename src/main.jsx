d9c2dffeed9b2bd648d653f5303b05531a110b33
Juan Mateo HDL <22300015@uttt.edu.mx>
2025-10-01 20:39:50 -0600

    feat: agregar base de proyecto React con Tailwind + Router

---
+++ src/main.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import './index.css'
+
+// páginas
+import Home from './pages/Home.jsx'
+import Login from './pages/Login.jsx'
+import Empleados from './pages/Empleados.jsx'
+import Contratos from './pages/Contratos.jsx'
+import Asistencias from './pages/Asistencias.jsx'
+import Evaluaciones from './pages/Evaluaciones.jsx'
+import Reclutamiento from './pages/Reclutamiento.jsx'
+import NotFound from './pages/NotFound.jsx'
+
+const router = createBrowserRouter([
+  { path: '/', element: <Home /> },
+  { path: '/login', element: <Login /> },
+  { path: '/empleados', element: <Empleados /> },
+  { path: '/contratos', element: <Contratos /> },
+  { path: '/asistencias', element: <Asistencias /> },
+  { path: '/evaluaciones', element: <Evaluaciones /> },
+  { path: '/reclutamiento', element: <Reclutamiento /> },
+  { path: '*', element: <NotFound /> },
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+)
